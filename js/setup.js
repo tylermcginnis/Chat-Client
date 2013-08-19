7021ee9d807d6f8add88c0d1ad2de3b756f1bcd9
Hack Reactor Students <github@hackreactor.com>
2013-08-19 16:02:55 -0700

    add get and post request. right now getting rick rolled but will fix soon.

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,27 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+$.ajax('https://api.parse.com/1/classes/messages?order=-createdAt', {
+  contentType: 'application/json',
+  success: function(data){
+    $.each(data.results, function(i, value){
+      //$('#main').append('<div> Message: ' + value.text + '</div>');
+    })
+    console.log(data);
+  },
+  error: function(data) {
+    console.log('Ajax request failed');
+  }
+});
+
+var message = JSON.stringify({text: "tyler mcginnis"});
+
+$.ajax('https://api.parse.com/1/classes/messages', {
+    contentType: 'application/json',
+    type: 'POST',
+    data: message,
+    success: function(result) {
+        console.log('result: ',result);
+    }
+});
